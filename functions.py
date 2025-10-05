9bfe44a8c02dc8abaf6c5ad5f428f7235ac4fbcd
Saif <prncesaif84@gmail.com>
2025-10-05 15:07:27 +0600

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the lis of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg):
+    """Write the to-do items list in a text file."""
+    with open('todos.txt', 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+#print(__name__)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
